8fb959ca437c5a8b461ad0319f089927c54901f4
HoseinBL <hoseynbehkamal@gmail.com>
2016-02-05 05:25:34 +0330

    add filter

--- www/js/pages/ideas-list.js
+++ www/js/pages/ideas-list.js
@@ -10,12 +10,8 @@
     });
   }])
 
-  .controller('singleIdeaCtrl', ['$scope', 'ideasStorage', function ($scope, ideasStorage){
-    ng.
-    extend($scope, {
-      ideasList : ideasStorage.getIdeasList()
-
-    });
+  .controller('singleIdeaCtrl', ['$scope', 'ideasStorage', '$stateParams', function ($scope, ideasStorage, $stateParams){
+    $scope.i = ideasStorage.get($stateParams.id);
   }])
   ;
 
